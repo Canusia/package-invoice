dbdc80b2004f46bb6932f03821ec23e097f12cee
Avi <kadaji@gmail.com>
2026-04-13 15:02:40 +0000

    change high school code to use state code

--- invoice/forms/invoice.py
+++ invoice/forms/invoice.py
@@ -412,7 +412,7 @@ class EventInvoiceForm(forms.Form):
 
             invoice.term = data.get('term')
             invoice.highschool = highschool
-            invoice.number = data.get('invoice_number') + highschool.code
+            invoice.number = data.get('invoice_number') + highschool.state_code
 
             invoice.meta = {}
             invoice.meta['billing_contact_id'] = str(data.get('billing_contact').id)
@@ -633,7 +633,7 @@ class ApplyDEInvoiceForm(forms.Form):
 
             invoice.term = data.get('term')
             invoice.highschool = highschool
-            invoice.number = data.get('invoice_number') + highschool.code
+            invoice.number = data.get('invoice_number') + highschool.state_code
 
             invoice.meta = {}
             invoice.meta['billing_contact_id'] = str(data.get('billing_contact').id)
